fd38bc04f1f7041041b34de22e0d40168930fdea
tamagoyakii <rlawlgus4063@naver.com>
2022-04-30 22:01:06 +0900

    delete

--- main.c
+++ main.c
@@ -3,17 +3,7 @@
 #include "get_next_line.h"
 
 int main()
-{
-	// char * s = NULL;
-	// int	i;
-
-	// i = -1;
-	// while (s[++i] != '\0')
-	// {
-	// 	if (s[i] == '\n')
-	// 		return (i + 1);
-	// }
-	
+{	
 	int fd = open("ganadara.txt", O_RDONLY);
 	char *line;
 	int i = 1;
